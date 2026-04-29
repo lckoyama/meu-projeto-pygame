b100b52b76b12d9d23e12c166a6f83c13846fc10
Lucas Koyama <lucaskoyama.dev@gmail.com>
2026-04-29 00:25:13 -0300

    avanço

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 class MeuGame:
     def __init__(self):
         pg.init()
+        self.scroll = 0
         self.window = pg.display.set_mode((840, 480))
         pg.display.set_caption("Meu Jogo")
         self.clock = pg.time.Clock()
@@ -14,10 +15,12 @@ class MeuGame:
         pg.mixer.music.load("data/8bit_bossa.mp3")
         pg.mixer.music.play(-1)
         
-        # som de passo
+        # sons
         self.walk_sound = pg.mixer.Sound("data/passos.wav")
         self.walk_channel = pg.mixer.Channel(1) 
-       
+        self.jump_sound = pg.mixer.Sound("data/pulo.wav")
+        self.jump_sound.set_volume(0.1) 
+            
         # física
         self.gravity = 1
 
@@ -27,14 +30,17 @@ class MeuGame:
         self.player_pos = [50, 50]
         self.player_jump_force = -16
         self.player_vertical_speed = 0
-
-      
+        
+        
+        
+        self.jump_l = [pg.transform.scale(pg.image.load(f"data/jump_left.png"), (64, 64))]
+        self.jump_r = [pg.transform.scale(pg.image.load(f"data/jump_right.png"), (64, 64))]
         self.idle_r = [pg.transform.scale(pg.image.load(f"data/idle_right_{i}.png"), (64, 64)) for i in [1, 2]]
         self.idle_l = [pg.transform.scale(pg.image.load(f"data/idle_left_{i}.png"), (64, 64)) for i in [1, 2]]
         self.walk_r = [pg.transform.scale(pg.image.load(f"data/walk_right_{i}.png"), (64, 64)) for i in range(1, 5)]
         self.walk_l = [pg.transform.scale(pg.image.load(f"data/walk_left_{i}.png"), (64, 64)) for i in range(1, 5)]
 
-        self.background = pg.transform.scale(pg.image.load("data/background1.png"), (840, 540))
+        self.background = pg.transform.scale(pg.image.load("data/background.png"), (840, 540))
         self.ground = pg.transform.scale(pg.image.load("data/pedra.png"), (64, 64))
 
     def player_collider(self):
@@ -51,6 +57,7 @@ class MeuGame:
         return False
 
     def move(self, keys):
+        
         moving = False
         on_ground = self.player_vertical_speed == 0 
 
@@ -58,66 +65,71 @@ class MeuGame:
             self.player_pos[0] -= 5
             if self.player_collider(): self.player_pos[0] += 5
             else:
-                self.player_animation = 3
+                self.player_animation = 1 
+                if on_ground: self.player_animation = 3
                 moving = True
+
         elif keys[pg.K_d]:
             self.player_pos[0] += 5
             if self.player_collider(): self.player_pos[0] -= 5
             else:
-                self.player_animation = 2  
+                self.player_animation = 0 
+                if on_ground: self.player_animation = 2
                 moving = True
 
-        
+        # Som de passos
         if moving and on_ground:
-          
             if not self.walk_channel.get_busy():
                 self.walk_channel.play(self.walk_sound, loops=-1)
         else:
-            
             self.walk_channel.stop()
 
-        # pulo
-        if keys[pg.K_SPACE] and self.player_vertical_speed == 0:
-            self.player_vertical_speed = self.player_jump_force
-            self.walk_channel.stop()
 
-        # idle logic
-        if not moving:
+        # Idle logic    
+        if not moving and on_ground:
             if self.player_animation == 2: self.player_animation = 0
             elif self.player_animation == 3: self.player_animation = 1
 
     def player(self):
-        # Gravidade
+        
+       
+       
         self.player_vertical_speed += self.gravity
         self.player_pos[1] += self.player_vertical_speed
 
         if self.player_collider():
+            
             self.player_pos[1] -= self.player_vertical_speed
             self.player_vertical_speed = 0
 
         # --- LÓGICA DE ANIMAÇÃO ---
         self.player_animation_frame += 0.05
         
-        if self.player_animation == 0: 
-            img_list = self.idle_r
-        elif self.player_animation == 1: 
-            img_list = self.idle_l
-        elif self.player_animation == 2:
-            img_list = self.walk_r
-        elif self.player_animation == 3:
-            img_list = self.walk_l
+        # 1. Determina a lista base
+        if self.player_animation == 0: img_list = self.idle_r
+        elif self.player_animation == 1: img_list = self.idle_l
+        elif self.player_animation == 2: img_list = self.walk_r
+        elif self.player_animation == 3: img_list = self.walk_l
 
-        if self.player_animation_frame >= len(img_list):
-            self.player_animation_frame = 0
+        
+        if self.player_vertical_speed != 0:
+            if self.player_animation in [0, 2]:
+                image_to_draw = self.jump_r[0]
+            else:
+                image_to_draw = self.jump_l[0]
+        else:
+           
+            if self.player_animation_frame >= len(img_list):
+                self.player_animation_frame = 0
+            image_to_draw = img_list[int(self.player_animation_frame)]
 
-        self.window.blit(img_list[int(self.player_animation_frame)], self.player_pos)
+        self.window.blit(image_to_draw, self.player_pos)
 
     def draw_map(self):
         for y in range(len(self.map)):
             for x in range(len(self.map[0])):
                 if self.map[y][x] != "":
-                    self.window.blit(self.ground, (x * 64, y * 64))
-
+                    self.window.blit(self.ground, (x * 64 - self.scroll, y * 64))
 # ---------------- LOOP ----------------
 jogo = MeuGame()
 
@@ -126,7 +138,15 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+        
+        # COLOQUE O PULO AQUI
         if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                if jogo.player_vertical_speed == 0:
+                    jogo.player_vertical_speed = jogo.player_jump_force
+                    jogo.jump_sound.play() 
+                    jogo.player_pos[1] -= 2
+            
             if event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
